86b8449b09d43dba1568d992c7c04e0af7d432fd
Sangram Chavan <sangramchavan16@gmail.com>
2017-01-11 01:02:11 +0530

    #

---
+++ entertainment.py
@@ -0,0 +1,49 @@
+import media
+import fresh_tomatoes
+
+chakras = media.Movie("7 Chakras",
+                      "How To Open Your 7 Chakras Explained",
+                      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTM3MTc3OTc0NF5BMl5BanBnXkFtZTcwOTQ0OTM1MQ@@._V1._CR34,0,295,440_UX182_CR0,0,182,268_AL__QL50.jpg",
+                      "https://www.youtube.com/watch?v=StrbppmsZJw"
+                      )
+#print(chakras.storyline)
+
+chakras1 = media.Movie("7 Chakras",
+                      "How To Open Your 7 Chakras Explained",
+                      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTM3MTc3OTc0NF5BMl5BanBnXkFtZTcwOTQ0OTM1MQ@@._V1._CR34,0,295,440_UX182_CR0,0,182,268_AL__QL50.jpg",
+                      "https://www.youtube.com/watch?v=StrbppmsZJw"
+                      )
+#print(chakras1.storyline)
+
+chakras2 = media.Movie("7 Chakras",
+                      "How To Open Your 7 Chakras Explained",
+                      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTM3MTc3OTc0NF5BMl5BanBnXkFtZTcwOTQ0OTM1MQ@@._V1._CR34,0,295,440_UX182_CR0,0,182,268_AL__QL50.jpg",
+                      "https://www.youtube.com/watch?v=StrbppmsZJw"
+                      )
+#print(chakras2.storyline)
+
+chakras3 = media.Movie("7 Chakras",
+                      "How To Open Your 7 Chakras Explained",
+                      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTM3MTc3OTc0NF5BMl5BanBnXkFtZTcwOTQ0OTM1MQ@@._V1._CR34,0,295,440_UX182_CR0,0,182,268_AL__QL50.jpg",
+                      "https://www.youtube.com/watch?v=StrbppmsZJw"
+                      )
+#print(chakras3.storyline)
+
+chakras4 = media.Movie("7 Chakras",
+                      "How To Open Your 7 Chakras Explained",
+                      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTM3MTc3OTc0NF5BMl5BanBnXkFtZTcwOTQ0OTM1MQ@@._V1._CR34,0,295,440_UX182_CR0,0,182,268_AL__QL50.jpg",
+                      "https://www.youtube.com/watch?v=StrbppmsZJw"
+                      )
+
+chakras5 = media.Movie("7 Chakras",
+                      "How To Open Your 7 Chakras Explained",
+                      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTM3MTc3OTc0NF5BMl5BanBnXkFtZTcwOTQ0OTM1MQ@@._V1._CR34,0,295,440_UX182_CR0,0,182,268_AL__QL50.jpg",
+                      "https://www.youtube.com/watch?v=StrbppmsZJw"
+                      )
+
+movies = [chakras, chakras1, chakras2, chakras3, chakras4, chakras5]
+fresh_tomatoes.open_movies_page(movies)
+#chakras.show_trailer()
+
+
+
